8be79b29e5aaaa181b3482a573f1886aea52828d
Dijana98 <dijana.alanovic@gmail.com>
2021-10-29 23:47:53 +0200

    first commit

---
+++ 11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main (){
+  int a,b;
+  scanf("%d %d\n", &a,&b);
+  printf("%d + %d = %d", a, b, a+b);
+
+
+
+  return 0;
+}
+// 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
